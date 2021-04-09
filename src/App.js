9c7c15a5d4fece7f0029e5d5a3eeda52b4185b46
Ali-consciousroad <ali_dindar@live.be>
2021-04-09 18:58:02 +0200

    Components Part 2: Use a list of card to display the info. in the menu component and the details of a selected dish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { DISHES } from './shared/dishes';
 import Menu from './components/MenuComponent';
 import './App.css';
 
   class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        dishes: DISHES
+      };
+    }
     render(){
       return (
         <div className="App">
@@ -15,7 +22,7 @@ import './App.css';
           </Navbar>
 
           {/* Display the MenuComponent.js content */}
-          <Menu />
+          <Menu dishes={this.state.dishes}/>
 
           <header className="App-header">
 
