d8b9fb1c0079b417380385fbf9d26a8a71719be4
Ali-consciousroad <ali_dindar@live.be>
2021-04-09 18:37:55 +0200

    Configuring React update: Update the App.js code to match the older version of React.js of this course to make next steps easier to follow

--- src/App.js
+++ src/App.js
@@ -1,57 +1,53 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
 
-function App() {
-  return (
+  class App extends Component {
+    render(){
+      return (
+        <div className="App">
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
 
+          {/* Display the MenuComponent.js content */}
+          <Menu />
 
-    
-    <div className="App">
+          <header className="App-header">
 
-      
+          <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            
+            
+          </header>
 
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <body className="App-body">
+
+        
+
+            <p>This is my body text</p>
+
+          </body>
+          
+          
         </div>
-      </Navbar>
-
-      {/* Display the MenuComponent.js content */}
-      <Menu />
-
-      <header className="App-header">
-
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-        
-      </header>
-
-      <body className="App-body">
-
-    
-
-        <p>This is my body text</p>
-
-      </body>
-      
-      
-    </div>
-  )
-}
-
+      );
+    }
+  }
 
 export default App;
   
