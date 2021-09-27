2b5560f58a25625bfc4136d5ee7b3b45597ad8fe
Ali-consciousroad <ali_dindar@live.be>
2021-09-27 18:13:13 +0200

    Add a few comments about lessons regarding components from w1

--- src/App.js
+++ src/App.js
@@ -1,60 +1,51 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import { DISHES } from './shared/dishes';
-import Menu from './components/MenuComponent';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+import { DISHES } from "./shared/dishes";
+import Menu from "./components/MenuComponent";
+import "./App.css";
 
-  class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        dishes: DISHES
-      };
-    }
-    render(){
-      return (
-        <div className="App">
-          <Navbar dark color="primary">
-            <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-          </Navbar>
-
-          {/* Display the MenuComponent.js content */}
-          <Menu dishes={this.state.dishes}/>
-
-          <header className="App-header">
-
-          <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            
-            
-          </header>
-
-          <body className="App-body">
-
-        
-
-            <p>This is my body text</p>
-
-          </body>
-          
-          
-        </div>
-      );
-    }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
   }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+
+        {/* Display the MenuComponent.js content */}
+        {/* Add the dishes as props to the Menu component */}
+        <Menu dishes={this.state.dishes} />
+
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+        <body className="App-body">
+          <p>This is my body text</p>
+        </body>
+      </div>
+    );
+  }
+}
 
 export default App;
-  
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,67 +1,82 @@
-import React, { Component } from 'react';
-import { Media } from 'reactstrap';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+import React, { Component } from "react";
+import { Media } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    // Initialize the state
+    this.state = {
+      selectedDish: null,
+    };
+  }
 
-        this.state = {
-            selectedDish: null
-        }
-    }
+  onDishSelect(dish) {
+    // Change the state with setState when the user click on a card
+    this.setState({ selectedDish: dish });
+  }
 
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
-    }
+  // Render the dish when the user click on card 
+  renderDish(dish) {
+    if (dish != null)
+      return (
+        <Card>
+          {/* Use the same idea than below to display a card image */}
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    else return <div></div>;
+  }
 
-    renderDish(dish) {
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return(
-                <div></div>
-            );
-    }
-
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-              <div  className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
-            );
-        });
-
-        return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                </div>
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    // Logic
+    // Map over each element of the dishes array and display them inside cards 
+    /* The state, data has been moved up so we now need to use this.props 
+       instead of this.state for the menu fonction */
+    // We can use the props here, the props is already in the class constructor
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <div className="col-12 col-md-5 m-1">
+          {/* We now use Bootstrap Card instead of Bootstrap Media for a better UI */}
+          {/* Key helps React identify each element of the Array uniquely and update only that one when needed */}
+          {/* Add here an event handler to respond to the click event */}
+          <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+    // View of the component
+    return (
+      <div className="container">
+        {/* Bootstrap class */}
+        <div className="row">
+          {menu}
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {/* Render the dish selected by the user | Why do we use state instead of props here? */}
+            {this.renderDish(this.state.selectedDish)}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Menu;
