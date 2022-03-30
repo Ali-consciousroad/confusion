13755450426325274385c6ae0db5ff73d53c4017
Ali-consciousroad <ali_dindar@live.be>
2022-03-30 15:30:38 +0200

    2. Code modularization: Add the dish details in the DishdetailComponent and use that component inside the MenuComponent to display the details for the selected dish, clean code

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,81 +4,110 @@
 - Pass the selected dish infor as props to the DishdetailComponent (from the MenuComponent?)
 */
 
-import React, { Component } from 'react';
-import { Media } from 'reactstrap';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
-
+import React, { Component } from "react";
+import { Media } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 
 class DishDetail extends Component {
-    /* 
+  // LOGIC
+
+  // Constructor
+  /* 
     Mandatory to call the super(props) method 
     before implementing a constructor with React
     */
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedDetail: null
-        }
+  constructor(props) {
+    super(props);
+  }
+
+  // state update
+  // onDishSelect(dish) {
+  //     this.setState({ selectedDish: dish});
+  // }
+
+  // function rendering the selected dish
+  renderDish(dish) {
+  /* Return a card if the dish is not null 
+    Display image, name and description of the card */
+    if (dish != null)
+      return (
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+            {/* <CardText>{dish.comments}</CardText> */}
+          </CardBody>
+        </Card>
+      );
+    /* Return an empty div if dish == null */
+    else {
+      return <div></div>;
     }
+  }
 
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
-    }
+  // Display comments method
+  // renderComment(dish) {
+  //   console.log("hello world");
+  //   if (dish != null)
+  //     return (
+  //       <Card>
+  //         <CardBody>
+  //           <CardText>{dish.comments}</CardText>
+  //         </CardBody>
+  //       </Card>
+  //     );
+  //   /* Return an empty div if dish == null */
+  //   else {
+  //     return <div></div>;
+  //   }
+  // }
 
-          // Display comments method
-          renderComment(dish){
-            console.log("hello world");
-            if (dish != null)
-                return(
-                    <Card>
-                        <CardBody>
-                          <CardText>{dish.comments}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-            else
-                {/* Return an empty div if dish == null */}
-                return(
-                    <div></div>
-                );
-          }
+  // Return a <div> from the render() function
 
-          // Return a <div> from the render() function
-    
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-              <div className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                  </CardImgOverlay>
-                </Card>
-              </div>
-            );
-        });
-        
-        return (
-            <div className="container">
-                <div className="row">
-                    {menu}
-                    { console.log("hello world 1") }
-                </div>
-                <div className="row">
-                  <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                  <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.state.selectedDish.comments)}
-                  </div>
-                </div>
-                
-            </div> 
-        );
-    }  
+  render() {
+    console.log(this.props);
+    // LOGIC
+    // Map over all the comments of a dish 
+    // const comment = this.props.dishes.map((dish) => {
+    //     return (
+    //       <div  className="col-12 col-md-5 m-1">
+    //         <Card key={dish.id}
+    //               /* Make the menu cards responsive to clicks */
+    //               onClick={() => this.onDishSelect(dish)}>
+    //           <CardImg width="100%" src={dish.image} alt={dish.name} />
+    //           <CardImgOverlay>
+    //               <CardTitle>{dish.name}</CardTitle>
+    //           </CardImgOverlay>
+    //         </Card>
+    //       </div>
+    //     );
+    // });
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {console.log("hello world 1")}
+            {/* Display the selected dish inside a card */}
+            {this.renderDish(this.props.dish)}
+          </div>
+            {/* Display the commments inside a card */}
+          <div className="col-12 col-md-5 m-1">
+          {console.log("hello world 2")}
+            {this.renderDish(this.props.comment)}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 // DishDetail class is now allowed to be imported
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,147 +1,94 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // Import the card component from reactstrap
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle } from 'reactstrap';
-import DishDetail from './DishdetailComponent';
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+import DishDetail from "./DishdetailComponent";
 
 class Menu extends Component {
-    // Required for class-based components
-    constructor(props) {
-        super(props);
-        /* State object: Define the state for the Menu component
+  // Required for class-based components
+  constructor(props) {
+    super(props);
+    /* State object: Define the state for the Menu component
            (properties for the component) */
-        this.state = {
-            /* 
-                Make the pictures responsive to clicks
-                Initial state: null
-             */
-            selectedDish: null
-        }
-    }
+    this.state = {
+      // Make the pictures responsive to clicks. Initial state: null
+      selectedDish: null,
+      selectedComment: null,
+    };
+  }
 
-    // LOGIC
-    /* 
+  // LOGIC
+  /* 
         Event Handler 
         - Select the dish with the onDishSelect method
         - Use dish as the parameter
         - Change the state of our component NOTE: always use the .setState method to do that
     */
-    // Modifiy the state with setState with the selected dish  
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish});
-    }
+  // state update
+  onDishSelect(dish, comment) {
+    this.setState({
+      selectedDish: dish,
+      selectedComment: comment,
+    });
+  }
 
-    // VIEW
-    // Render the selected dish 
-    renderDish(dish) {
-        {/* 
-            Return a card if the dish is not null 
-            Display image, name and description of the card
-        */}
-        if (dish != null)
-            return(
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                      <CardTitle>{dish.name}</CardTitle>
-                      <CardText>{dish.description}</CardText>
-                      {/* <CardText>{dish.comments}</CardText> */}
-                    </CardBody>
-                </Card>
-            );
-        else
-            {/* Return an empty div if dish == null */}
-            return(
-                <div></div>
-            );
-    }
-
-    // Render the comments of the selected dish
-    renderComment(dish){
-        console.log("hello world");
-        if (dish != null)
-            return(
-                <Card>
-                    <CardBody>
-                      <CardText>{dish.comments}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            {/* Return an empty div if dish == null */}
-            return(
-                <div></div>
-            );
-      }
-    // VIEW
-    // Required for any class-based component 
-    render() {
-        {/*
-            - Use of this.props instead of this.state, props allows the child component Menu 
-            to read values from the App parent component. Data is taken down from the App component. 
-            - Use card component instead of the media object from Reactstrap
-        */}
-
-        // Map over all the dishes to display the menu
-        console.log(this.props);
-        const menu = this.props.dishes.map((dish) => {
-            return (
-              <div className="col-12 col-md-5 m-1">
-                <Card key={dish.id}
-                      /* Make the menu cards responsive to clicks 
+  // VIEW
+  // Required for any class-based component
+  render() {
+    // TEST log the props
+    console.log(this.props);
+    // LOGIC
+    /* menu function: Map over all the dishes to display the menu */
+    /* - Use of this.props instead of this.state, props allows the child component Menu to read values from the App parent component. 
+       Data is taken down from the App component. 
+       - Use card component instead of the media object from Reactstrap */
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <Card
+            key={dish.id}
+            /* Make the menu cards responsive to clicks 
                          on click modifiy the state by making a call to our event handler */
-                      onClick={() => this.onDishSelect(dish)}>
-                  <CardImg width="100%" src={dish.image} alt={dish.name} />
-                  <CardImgOverlay>
-                      <CardTitle>{dish.name}</CardTitle>
-                      {/* <CardText>{DISHES[comments.comment]}</CardText> */}
-                  </CardImgOverlay>
-                </Card>
-              </div>
-            );
-        });
+            onClick={() => this.onDishSelect(dish)}
+          >
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+              {/* <CardText>{DISHES[comments.comment]}</CardText> */}
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
 
-        // const comment = this.props.dishes.map((dish) => {
-        //     return (
-        //       <div  className="col-12 col-md-5 m-1">
-        //         <Card key={dish.id}
-        //               /* Make the menu cards responsive to clicks */
-        //               onClick={() => this.onDishSelect(dish)}>
-        //           <CardImg width="100%" src={dish.image} alt={dish.name} />
-        //           <CardImgOverlay>
-        //               <CardTitle>{dish.name}</CardTitle>
-        //           </CardImgOverlay>
-        //         </Card>
-        //       </div>
-        //     );
-        // });
-
-        return (
-            <div className="container">
-                {/* First row: Display the menu */}
-                <div className="row">
-                    {menu}
-                </div>
-                {/* Second row: Display the content */}
-                <div className="row">
-                  <div  className="col-12 col-md-5 m-1">
-                    {/* Display the selected card */}
-                    {/* First way to do it */}
-                    {/* {this.renderDish(this.state.selectedDish)} */}
-                    {/* Second way to do it with code modularization by using DishDetail 
-                    Make the selected dishes available as props to DishDetail */}
-                    <DishDetail dish={this.state.selectedDish} /> 
-                    {/* <DishDetail dish={this.props.selectedDish} comments={this.props.comments} /> */} 
-                  </div>
-                  {/* Display the comment inside the second row */}
-                  
-                  <div className="col-12 col-md-5 m-1">
-                    {/* {this.renderComment(this.state.selectedDish)} */}
-                  </div> 
-                </div>
-            </div>
-        );
-    }
+    // VIEW
+    return (
+      <div className="container">
+        {/* First row: Display the menu */}
+        <div className="row">{menu}</div>
+        {/* Second row: Display the content */}
+        <div className="row">
+          {/* Display the selected card */}
+          {/* Second way to do it with code modularization by using DishDetail 
+                    Make the selected dishes available as props to the DishDetailComponent */}
+          <DishDetail
+            dish={this.state.selectedDish}
+            comment={this.state.selectedComment}
+          />
+          {/* <DishDetail dish={this.state.selectedDish} comments={this.state.comments} /> */}
+          <div className="col-12 col-md-5 m-1">
+            {/* {this.renderComment(this.state.selectedDish)} */}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Menu;
