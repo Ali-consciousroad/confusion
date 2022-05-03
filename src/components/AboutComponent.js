b54922f64b474e3e7a2f1ae2378a62428f5f1806
Ali-consciousroad <ali_dindar@live.be>
2022-05-03 22:03:30 +0200

    Implement the RenderLeader function using the leader information as parameter, the reactstrap Media component and use it inside the AboutComponent() function to render the list of leaders and their information

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
+import { Card, CardImg, CardTitle, CardBody, CardHeader, CardImgOverlay, Breadcrumb, BreadcrumbItem, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
+function RenderLeader({ leader}) {
+    return (
+        <div key={leader.id} className="col-12 mt-5">
+            <Media tag="li">
+                <Media left middle> 
+                    <Media object src={leader.image} alt={leader.name} />
+                </Media>
+                    <Media body className="ml-5">
+                        <Media heading>
+                            {leader.name}
+                        </Media>
+                        <p>{leader.designation}</p>
+                        <p>{leader.description}</p>
+                    </Media>
+            </Media>
+        </div>
+    );
+}
 
 const About = props => {
     const leaders = props.leaders.map((leader) => {
         return (
-            <p>Leader {leader.name}</p>
+            // <p>Leader {leader.name}</p>
+            <RenderLeader leader={leader} onClick={props.onClick}/>
         );
     });
 
