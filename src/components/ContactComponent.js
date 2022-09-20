6f2251ffcffe4e008e0c3dff61718cfaef124be7
Ali-consciousroad <ali_dindar@live.be>
2022-09-20 18:07:56 +0200

    Allow firstname value added in the feedback form to be saved in the JSON db

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -23,7 +23,7 @@ class Contact extends Component {
         alert('Current State is: ' + JSON.stringify(values));
         // Not needed with React Redux Form 
         // event.preventDefault();
-        this.props.postFeedback(this.props.firstname, values.lastname, values.telnum, values.email, values.agree, values.contactType, values.message);
+        this.props.postFeedback(values.firstname, values.lastname, values.telnum, values.email, values.agree, values.contactType, values.message);
         this.props.resetFeedbackForm();
     }
 
